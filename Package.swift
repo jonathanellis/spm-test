b89c3c6e19cda9b415a29c0325c7eb347c02cce1
Jonathan Ellis <jon@than.biz>
2020-11-18 12:41:04 +0000

    use master branch

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "SocketIO", url: "https://github.com/iProov/socket.io-client-swift", .upToNextMajor(from: "15.2.0")),
+        .package(name: "SocketIO", url: "https://github.com/iProov/socket.io-client-swift", .branch("master")),
     ],
     targets: [
         .target(
